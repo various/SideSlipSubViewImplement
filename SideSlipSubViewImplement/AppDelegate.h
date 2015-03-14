9bc1dcb27dd03678a2d71b7b8ab1783a2b87dc2b
Various <apple123@mail.ustc.edu.cn>
2015-03-14 10:50:35 +0800

    init

---
+++ SideSlipSubViewImplement/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SideSlipSubViewImplement
+//
+//  Created by Tim Geng on 3/14/15.
+//  Copyright (c) 2015 GF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
